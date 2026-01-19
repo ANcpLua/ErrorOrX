9fce1e53b99ecdad159a9534468d310e6eb46777
ancplua <anfh22@outlook.com>
2026-01-20 00:26:28 +0100

    build: Add missing ProjectReference and AwesomeAssertions to Mutation Tests

---
+++ tests/ErrorOrX.Tests/Errors/Error.NullMetadataTests.cs
@@ -0,0 +1,37 @@
+namespace ErrorOrX.Tests.Errors;
+
+public class ErrorNullMetadataTests
+{
+    [Fact]
+    public void Equals_WhenMetadataContainsNullValue_ShouldNotThrow()
+    {
+        // Arrange
+        var metadata = new Dictionary<string, object?> { ["Key"] = null };
+        var error1 = Error.Validation("Code", "Description", metadata!);
+        var error2 = Error.Validation("Code", "Description", new Dictionary<string, object?> { ["Key"] = null }!);
+
+        // Act
+        // This is expected to throw NullReferenceException in the current implementation
+        var act = () => error1.Equals(error2);
+
+        // Assert
+        // We initially expect this to FAIL (i.e. it WILL throw) to prove the bug.
+        // After fix, it should NotThrow and return True.
+        act.Should().NotThrow();
+        act().Should().BeTrue();
+    }
+
+    [Fact]
+    public void Equals_WhenOneMetadataValueIsNullAndOtherIsNot_ShouldReturnFalse()
+    {
+        // Arrange
+        var error1 = Error.Validation("Code", "Description", new Dictionary<string, object?> { ["Key"] = null }!);
+        var error2 = Error.Validation("Code", "Description", new Dictionary<string, object?> { ["Key"] = "Value" }!);
+
+        // Act
+        var result = error1.Equals(error2);
+
+        // Assert
+        result.Should().BeFalse();
+    }
+}
